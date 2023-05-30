a2e9ec4bad18c99f9345a8f91b1db305b262a78b
siggu <106001755+siggu@users.noreply.github.com>
2023-05-30 09:30:08 +0900

    CSS in Javascript part Two

--- nomadcoders/바닐라 JS로 크롬 앱 만들기/MOMENTUM/app.js
+++ nomadcoders/바닐라 JS로 크롬 앱 만들기/MOMENTUM/app.js
@@ -2,42 +2,13 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    const colors = ["red", "green", "blue", "pink", "white", "purple"];
-    let num = Math.floor(Math.random()*colors.length);
-
-    h1.style.color = colors[num];
+    const clickedClass = "clicked";
+    if (h1.className === clickedClass) {
+        h1.className = "";
+    } else {
+        h1.className = clickedClass;
+    }
 }
 
-function handleMouseenter() {
-    h1.innerText = "Mouse is here!";
-}
+h1.addEventListner("click", handleTitleClick);
 
-function handleMouseLeave() {
-    h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-    alert("copier!");
-}
-
-function handleWindowOffline() {
-    alert("SOS no wifi");
-}
-
-function handleWindowOnline() {
-    alert("All Goood!");
-}
-
-h1.onclick = handleTitleClick;
-h1.onmouseenter = handleMouseenter;
-h1.onmouseleave = handleMouseLeave;
-
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
