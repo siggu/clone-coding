bd49491fcc44f2923626f9279f11215ae690a6f2
siggu <106001755+siggu@users.noreply.github.com>
2023-06-06 15:01:50 +0900

    Delete Form Submission.js

--- nomadcoders/바닐라 JS로 크롬 앱 만들기/MOMENTUM/js/Form Submission.js
+++
@@ -1,9 +0,0 @@
-const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
-
-function handleloginBtnClick() {
-    const username = loginInput.value;
-    console.log(username);
-}
-
-loginButton.addEventListener("click", handleloginBtnClick);
