78a384f885f90f1f339b520121a7cc8981420248
siggu <m0_w_0m@naver.com>
2023-06-06 15:30:35 +0900

    4.2 Events

--- nomadcoders/바닐라 JS로 크롬 앱 만들기/MOMENTUM/app.js
+++ nomadcoders/바닐라 JS로 크롬 앱 만들기/MOMENTUM/app.js
@@ -1,9 +1,14 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginbutton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-    const username = loginInput.value;
-    console.log(username);
+function onLoginSubmit(event){
+    event.preventDefault();  // preventDefault(): 어떤 event의 기본 행동이든지 발생되지 않도록 막음
+    console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
+
+/*
+loginForm을 "submit" 하게 되면 "기본적으로" 페이지를 새로고침 하게 된다.
+하지만, `preventDefault()` 함수를 추가해주면 브라우저의 기본 동작을 막을 수 있다.
+*/
