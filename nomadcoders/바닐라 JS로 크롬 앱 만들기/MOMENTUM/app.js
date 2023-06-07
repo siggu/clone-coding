3c28c7ed3f7bf009f51ad87c1d23c0e5af0b3f40
siggu <m0_w_0m@naver.com>
2023-06-07 09:57:09 +0900

    4.4 Getting Username

--- nomadcoders/바닐라 JS로 크롬 앱 만들기/MOMENTUM/app.js
+++ nomadcoders/바닐라 JS로 크롬 앱 만들기/MOMENTUM/app.js
@@ -1,14 +1,18 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
+
 
 function onLoginSubmit(event){
-    event.preventDefault();  // preventDefault(): 어떤 event의 기본 행동이든지 발생되지 않도록 막음
-    console.log(loginInput.value);
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    greeting.innerText = `Hello ${username}`;
+    // greeting.innerText = "Hello " + username; 이거랑 같은 역할을 해준다.
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
 
-/*
-loginForm을 "submit" 하게 되면 "기본적으로" 페이지를 새로고침 하게 된다.
-하지만, `preventDefault()` 함수를 추가해주면 브라우저의 기본 동작을 막을 수 있다.
-*/
+
+loginForm.addEventListener("submit", onLoginSubmit);
