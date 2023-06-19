4fdf1074e01903b0a8d389ff4a5a25429b5c6e4a
siggu <m0_w_0m@naver.com>
2023-06-19 17:53:02 +0900

    Saving ToDos

--- 바닐라 JS로 크롬 앱 만들기/MOMENTUM/js/todo.js
+++ 바닐라 JS로 크롬 앱 만들기/MOMENTUM/js/todo.js
@@ -2,6 +2,16 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDo = [];
+
+function saveToDos() {
+    localStorage.setItem("todos", JSON.stringify(toDo));
+    /*  
+        localStorage에는 배열이 저장되지 않고 텍스트만 저장되기 때문에 
+        JSON.stringify를 이용해 객체, 배열을 문자열로 바꿔준다.
+    */
+}
+
 function deleteToDo(event) {
     const li = event.target.parentElement;
     li.remove();
@@ -23,7 +33,9 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDo.push(newTodo);
     paintToDo(newTodo);
+    saveToDos(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
