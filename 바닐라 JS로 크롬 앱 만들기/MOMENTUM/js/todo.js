91bcb90cdfcb28e86d8a8511f306b991165998f4
siggu <m0_w_0m@naver.com>
2023-06-19 18:13:38 +0900

    Loading ToDos part One

--- 바닐라 JS로 크롬 앱 만들기/MOMENTUM/js/todo.js
+++ 바닐라 JS로 크롬 앱 만들기/MOMENTUM/js/todo.js
@@ -2,10 +2,12 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
 const toDo = [];
 
 function saveToDos() {
-    localStorage.setItem("todos", JSON.stringify(toDo));
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDo));
     /*  
         localStorage에는 배열이 저장되지 않고 텍스트만 저장되기 때문에 
         JSON.stringify를 이용해 객체, 배열을 문자열로 바꿔준다.
@@ -39,3 +41,22 @@ function handleToDoSubmit(event) {
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+/*
+function sayHello(item) {   // 실행되고 있는 item에 대한 정보도 알 수 있음
+    console.log("this is the turn of as", item);
+}
+함수를 사용하지 않고도 쓸 수 있는 방법이 있음
+*/
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+    const parseToDos = JSON.parse(savedToDos);
+    parseToDos.forEach((item) => console.log("this is the turn of", item));
+    /*
+    parsedToDos.forEach();  //  parsedToDos에는 array에 있는 각각의 item에 대해 
+    function을 한 개만 실행해줄 수 있는 forEach()가 있다.
+
+    arrow function을 이용해 함수를 사용하지 않고도 같은 역할을 할 수 있다.
+    */
+    
+}
