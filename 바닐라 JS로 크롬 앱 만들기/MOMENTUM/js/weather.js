48b37ffcebfea4c26c6bcee0c189c2ec7765dc90
siggu <m0_w_0m@naver.com>
2023-06-24 18:46:23 +0900

    Weather API

--- 바닐라 JS로 크롬 앱 만들기/MOMENTUM/js/weather.js
+++ 바닐라 JS로 크롬 앱 만들기/MOMENTUM/js/weather.js
@@ -1,7 +1,18 @@
+const API_KEY = "7de0e19f641884f5e504a5a463dd975b";
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in ", + lat + ", " + lng);
+    const lon = position.coords.longitude;
+    console.log("You live in ", + lat + ", " + lon);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY};`
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weatherContainer = document.querySelector("#weater span:first-child");
+            const city = document.querySelector("#weater span:last-child");
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        });
 }
 
 function onGeoError() {
