e77511c747be7ae14ac9632d48a11be75f0d9835
siggu <m0_w_0m@naver.com>
2023-08-04 17:26:08 +0900

    Post

--- Making_web-service/server.js
+++ Making_web-service/server.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(8080, function () {
     console.log('listening on 8080');
 });
 
-app.get('/pet', function (req, res) {
-    res.send('펫 용품을 쇼핑할 수 있는 페이지입니다.');
-});
-
-app.get('/beauty', function (요청, 응답) {
-    응답.send('뷰티 용품을 쇼핑할 수 있는 페이지 입니다.');
-});
-
 app.get('/', function (요청, 응답) {
     응답.sendFile(__dirname + '/index.html');
+});
+
+app.get('/write', function (요청, 응답) {
+    응답.sendFile(__dirname + '/write.html');
+});
+
+app.post('/add', function (요청, 응답) {
+    응답.send('전송완료');
+    console.log(요청.body.title);
+    console.log(요청.body.date);
 });
