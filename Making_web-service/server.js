d6eed59502a902fdd9b5d90c33136ac650206046
siggu <m0_w_0m@naver.com>
2023-08-06 21:50:52 +0900

    store data

--- Making_web-service/server.js
+++ Making_web-service/server.js
@@ -3,9 +3,25 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(8080, function () {
-    console.log('listening on 8080');
-});
+const MongoClient = require('mongodb').MongoClient;
+var db;
+
+MongoClient.connect(
+    'mongodb+srv://jeongmok:qwer1234@cluster0.v5rclej.mongodb.net/?retryWrites=true&w=majority',
+    { useUnifiedTopology: true },
+    function (에러, client) {
+        if (에러) return console.log(에러);
+        db = client.db('todoapp');
+
+        db.collection('post').insertOne({ 이름: 'john', _id: 100 }, function (에러, 결과) {
+            console.log('저장완료');
+        });
+
+        app.listen(8080, function () {
+            console.log('listening on 8080');
+        });
+    }
+);
 
 app.get('/', function (요청, 응답) {
     응답.sendFile(__dirname + '/index.html');
