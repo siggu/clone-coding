fa7d91dcb87f4e71a62798b4cdcc08a225b2cc0b
siggu <m0_w_0m@naver.com>
2023-08-07 10:55:35 +0900

    counter

--- Making_web-service/server.js
+++ Making_web-service/server.js
@@ -8,18 +8,14 @@ var db;
 
 app.set('view engine', 'ejs');
 
-MongoClient.connect(
-    'mongodb+srv://jeongmok:qwer1234@cluster0.v5rclej.mongodb.net/?retryWrites=true&w=majority',
-    { useUnifiedTopology: true },
-    function (에러, client) {
-        if (에러) return console.log(에러);
-        db = client.db('todoapp');
+MongoClient.connect('mongodb+srv://jeongmok:qwer1234@cluster0.v5rclej.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true }, function (에러, client) {
+    if (에러) return console.log(에러);
+    db = client.db('todoapp');
 
-        app.listen(8080, function () {
-            console.log('listening on 8080');
-        });
-    }
-);
+    app.listen(8080, function () {
+        console.log('listening on 8080');
+    });
+});
 
 app.get('/', function (요청, 응답) {
     응답.sendFile(__dirname + '/index.html');
@@ -33,9 +29,17 @@ app.post('/add', function (요청, 응답) {
     응답.send('전송완료');
     console.log(요청.body.title);
     console.log(요청.body.date);
+    db.collection('counter').findOne({ name: '게시물 개수' }, function (에러, 결과) {
+        console.log(결과.totalPost);
 
-    db.collection('post').insertOne({ 제목: 요청.body.title, 날짜: 요청.body.date }, function (에러, 결과) {
-        console.log('저장완료');
+        var 총게시물개수 = 결과.totalPost;
+        db.collection('post').insertOne({ _id: 총게시물개수 + 1, 제목: 요청.body.title, 날짜: 요청.body.date }, function (에러, 결과) {
+            console.log('저장완료');
+
+            db.collection('counter').updateOne({ name: '게시물 개수' }, { $inc: { totalPost: 1 } }, function (에러, 결과) {
+                if (에러) return console.log(에러);
+            });
+        });
     });
 });
 
