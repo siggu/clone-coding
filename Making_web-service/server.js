1934f12401d3d7cd3ad4f4b242afd15c7e3c7ea5
siggu <m0_w_0m@naver.com>
2023-08-08 23:17:54 +0900

    Ajax

--- Making_web-service/server.js
+++ Making_web-service/server.js
@@ -27,15 +27,12 @@ app.get('/write', function (요청, 응답) {
 
 app.post('/add', function (요청, 응답) {
     응답.send('전송완료');
-    console.log(요청.body.title);
-    console.log(요청.body.date);
     db.collection('counter').findOne({ name: '게시물 개수' }, function (에러, 결과) {
         console.log(결과.totalPost);
-
         var 총게시물개수 = 결과.totalPost;
+
         db.collection('post').insertOne({ _id: 총게시물개수 + 1, 제목: 요청.body.title, 날짜: 요청.body.date }, function (에러, 결과) {
             console.log('저장완료');
-
             db.collection('counter').updateOne({ name: '게시물 개수' }, { $inc: { totalPost: 1 } }, function (에러, 결과) {
                 if (에러) return console.log(에러);
             });
@@ -50,4 +47,13 @@ app.get('/list', function (요청, 응답) {
             console.log(결과);
             응답.render('list.ejs', { posts: 결과 });
         });
+});
+
+app.delete('/delete', function (요청, 응답) {
+    console.log(요청.body);
+    요청.body._id = parseInt(요청.body._id);
+    db.collection('post').deleteOne(요청.body, function (에러, 결과) {
+        console.log('삭제완료');
+    });
+    응답.send('삭제완료');
 });
