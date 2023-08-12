8e73bbaf71618062fc1620c20aa6043aecf8882c
siggu <m0_w_0m@naver.com>
2023-08-12 22:26:33 +0900

    Inheritance

--- airbnb/main.py
+++ airbnb/main.py
@@ -1,11 +1,22 @@
-class Player:
-    def __init__(self, name, xp):
+class Human:
+    def __init__(self, name):
         self.name = name
-        self.xp = xp
 
     def say_hello(self):
         print(f"hello my name is {self.name}")
 
 
-nico = Player("nico", 1000)
-nico.say_hello()
+class Player(Human):
+    def __init__(self, name, xp):
+        self.xp = xp
+
+
+class Fan(Human):
+    def __init__(self, name, fav_team):
+        self.fav_team = fav_team
+
+
+nico_player = Player("nico", 10)
+nico_player.say_hello()
+nico_fan = Fan("nico_fan", "dontknow")
+nico_fan.say_hello()
