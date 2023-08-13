d94a490f7b035376e1506f7f34a3976884c50c69
siggu <m0_w_0m@naver.com>
2023-08-13 17:21:41 +0900

    super

--- airbnb/main.py
+++ airbnb/main.py
@@ -1,5 +1,6 @@
 class Human:
     def __init__(self, name):
+        print("human initialized")
         self.name = name
 
     def say_hello(self):
@@ -8,15 +9,15 @@ class Human:
 
 class Player(Human):
     def __init__(self, name, xp):
+        super.__init__(name)
         self.xp = xp
 
 
 class Fan(Human):
     def __init__(self, name, fav_team):
+        super().__init__(name)
         self.fav_team = fav_team
 
 
-nico_player = Player("nico", 10)
-nico_player.say_hello()
-nico_fan = Fan("nico_fan", "dontknow")
-nico_fan.say_hello()
+nico = Fan("nico", "blue")
+nico.say_hello()
